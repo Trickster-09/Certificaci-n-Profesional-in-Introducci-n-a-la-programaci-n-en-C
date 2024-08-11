c6c02a29879b4223e49cdb9862b5e914dd050ecf
Omar Ramos Soto <141699872+Trickster-09@users.noreply.github.com>
2024-08-11 15:29:38 -0600

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+double peso = 183.0, altura = 2.32;
+char sexo = 'M';
+int cp = 2764 ,hp = 159, caramelos = 266;
+
+printf("Sexo: %c \nCP %d \nHP: %d \nPeso: %lf \nAltura: %2lf \nCaramelos: %d", sexo, cp, hp, peso, altura, caramelos);
+
+return 0;
+}

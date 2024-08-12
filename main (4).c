1b950d8c3e1cbf7d499de66ae5944b97da88d4c9
Omar Ramos Soto <141699872+Trickster-09@users.noreply.github.com>
2024-08-12 10:35:05 -0600

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<limits.h>
+
+int main() {
+
+printf("Tamano variable Sexo: %ld\n", sizeof(char));
+printf("Tamano variable CP: %ld\n", sizeof(int));
+printf("Tamano variable HP: %ld\n", sizeof(int));
+printf("Tamano variable Peso: %ld\n", sizeof(char));
+printf("Tamano variable Altura: %ld\n", sizeof(double));
+printf("Tamano variable Caramelos: %ld\n", sizeof(int));
+
+return 0;
+}
